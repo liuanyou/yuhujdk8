d0483c86b13e266c960ccb70d7450242623f0c14
Anyou Liu <anyou.liu@convertlab.com>
2024-05-26 19:14:35 +0800

    feature:解决 cpu 架构相关编译报错 - 18. 解决编译 classes.cpp 语法报错

--- hotspot/src/share/vm/opto/mathexactnode.hpp
+++ hotspot/src/share/vm/opto/mathexactnode.hpp
@@ -149,6 +149,108 @@ public:
   virtual uint ideal_reg() const { return Op_RegFlags; }
 };
 
+class OverflowNode : public CmpNode {
+public:
+    OverflowNode(Node* in1, Node* in2) : CmpNode(in1, in2) {}
+
+    virtual uint ideal_reg() const { return Op_RegFlags; }
+    virtual const Type* sub(const Type* t1, const Type* t2) const;
+};
+
+class OverflowINode : public OverflowNode {
+public:
+    typedef TypeInt TypeClass;
+
+    OverflowINode(Node* in1, Node* in2) : OverflowNode(in1, in2) {}
+    virtual Node* Ideal(PhaseGVN* phase, bool can_reshape);
+    virtual const Type* Value(PhaseTransform* phase) const;
+
+    virtual bool will_overflow(jint v1, jint v2) const = 0;
+    virtual bool can_overflow(const Type* t1, const Type* t2) const = 0;
+};
+
+/*
+ * OverflowNode
+ */
+class OverflowLNode : public OverflowNode {
+public:
+    typedef TypeLong TypeClass;
+
+    OverflowLNode(Node* in1, Node* in2) : OverflowNode(in1, in2) {}
+    virtual Node* Ideal(PhaseGVN* phase, bool can_reshape);
+    virtual const Type* Value(PhaseTransform* phase) const;
+
+    virtual bool will_overflow(jlong v1, jlong v2) const = 0;
+    virtual bool can_overflow(const Type* t1, const Type* t2) const = 0;
+};
+
+class OverflowAddINode : public OverflowINode {
+public:
+    typedef AddINode MathOp;
+
+    OverflowAddINode(Node* in1, Node* in2) : OverflowINode(in1, in2) {}
+    virtual int Opcode() const;
+
+    virtual bool will_overflow(jint v1, jint v2) const;
+    virtual bool can_overflow(const Type* t1, const Type* t2) const;
+};
+
+class OverflowSubINode : public OverflowINode {
+public:
+    typedef SubINode MathOp;
+
+    OverflowSubINode(Node* in1, Node* in2) : OverflowINode(in1, in2) {}
+    virtual int Opcode() const;
+
+    virtual bool will_overflow(jint v1, jint v2) const;
+    virtual bool can_overflow(const Type* t1, const Type* t2) const;
+};
+
+class OverflowMulINode : public OverflowINode {
+public:
+    typedef MulINode MathOp;
+
+    OverflowMulINode(Node* in1, Node* in2) : OverflowINode(in1, in2) {}
+    virtual int Opcode() const;
+
+    virtual bool will_overflow(jint v1, jint v2) const;
+    virtual bool can_overflow(const Type* t1, const Type* t2) const;
+};
+
+class OverflowAddLNode : public OverflowLNode {
+public:
+    typedef AddLNode MathOp;
+
+    OverflowAddLNode(Node* in1, Node* in2) : OverflowLNode(in1, in2) {}
+    virtual int Opcode() const;
+
+    virtual bool will_overflow(jlong v1, jlong v2) const;
+    virtual bool can_overflow(const Type* t1, const Type* t2) const;
+};
+
+class OverflowSubLNode : public OverflowLNode {
+public:
+    typedef SubLNode MathOp;
+
+    OverflowSubLNode(Node* in1, Node* in2) : OverflowLNode(in1, in2) {}
+    virtual int Opcode() const;
+
+    virtual bool will_overflow(jlong v1, jlong v2) const;
+    virtual bool can_overflow(const Type* t1, const Type* t2) const;
+};
+
+class OverflowMulLNode : public OverflowLNode {
+public:
+    typedef MulLNode MathOp;
+
+    OverflowMulLNode(Node* in1, Node* in2) : OverflowLNode(in1, in2) {}
+    virtual int Opcode() const;
+
+    virtual bool will_overflow(jlong v1, jlong v2) const { return is_overflow(v1, v2); }
+    virtual bool can_overflow(const Type* t1, const Type* t2) const;
+
+    static bool is_overflow(jlong v1, jlong v2);
+};
 
 #endif
 
