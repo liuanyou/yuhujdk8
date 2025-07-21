191349e8a050ed56f228d09c67b5200115c449d4
Anyou Liu <anyou.liu@convertlab.com>
2025-07-21 22:12:29 +0800

    feature: refactor code of generate_call_stub and generate_catch_exception methods in yuhu stub generator

--- hotspot/src/cpu/aarch64/vm/yuhu/yuhu_stubGenerator_aarch64.cpp
+++ hotspot/src/cpu/aarch64/vm/yuhu/yuhu_stubGenerator_aarch64.cpp
@@ -24,161 +24,224 @@
 
 class YuhuStubGenerator : public YuhuStubCodeGenerator {
 private:
+    // Call stub stack layout word offsets from fp
+    enum call_stub_layout {
+        sp_after_call_off = -26,
+
+        d15_off            = -26,
+        d13_off            = -24,
+        d11_off            = -22,
+        d9_off             = -20,
+
+        r28_off            = -18,
+        r26_off            = -16,
+        r24_off            = -14,
+        r22_off            = -12,
+        r20_off            = -10,
+        call_wrapper_off   =  -8,
+        result_off         =  -7,
+        result_type_off    =  -6,
+        method_off         =  -5,
+        entry_point_off    =  -4,
+        parameter_size_off =  -2,
+        thread_off         =  -1,
+        fp_f               =   0,
+        retaddr_off        =   1,
+    };
+
     address generate_call_stub(address& return_address) {
+        assert((int)frame::entry_frame_after_call_words == -(int)sp_after_call_off + 1 &&
+               (int)frame::entry_frame_call_wrapper_offset == (int)call_wrapper_off,
+               "adjust this code");
+
+//        StubCodeMark mark(this, "StubRoutines", "call_stub");
         address start = __ current_pc();
 
-        // x0 -> call wrapper (&)
-        // x1 -> result (intptr_t*)
-        // x2 -> result type
-        // x3 -> method
-        // x4 -> entry point
-        // x5 -> parameters (intptr_t*)
-        // x6 -> parameter size (int)
+//        const Address sp_after_call(rfp, sp_after_call_off * wordSize);
 
-        /* open new stack frame */
+//        const Address call_wrapper  (rfp, call_wrapper_off   * wordSize);
+//        const Address result        (rfp, result_off         * wordSize);
+//        const Address result_type   (rfp, result_type_off    * wordSize);
+//        const Address method        (rfp, method_off         * wordSize);
+//        const Address entry_point   (rfp, entry_point_off    * wordSize);
+//        const Address parameter_size(rfp, parameter_size_off * wordSize);
+
+//        const Address thread        (rfp, thread_off         * wordSize);
+
+//        const Address d15_save      (rfp, d15_off * wordSize);
+//        const Address d13_save      (rfp, d13_off * wordSize);
+//        const Address d11_save      (rfp, d11_off * wordSize);
+//        const Address d9_save       (rfp, d9_off * wordSize);
+
+//        const Address r28_save      (rfp, r28_off * wordSize);
+//        const Address r26_save      (rfp, r26_off * wordSize);
+//        const Address r24_save      (rfp, r24_off * wordSize);
+//        const Address r22_save      (rfp, r22_off * wordSize);
+//        const Address r20_save      (rfp, r20_off * wordSize);
+
+        // stub code
+
+        address aarch64_entry = __ current_pc();
+
+        // set up frame and move sp to end of save area
         __ write_insts_enter();
-        __ write_inst("sub sp, x29, #0xd0");
-        /* save method parameters */
-        __ write_inst("stur x7, [x29, #-0x08]");
-        __ write_inst("stur x6, [x29, #-0x10]");
-        __ write_inst("stp x4, x5, [x29, #-0x20]");
-        __ write_inst("stp x2, x3, [x29, #-0x30]");
-        __ write_inst("stp x0, x1, [x29, #-0x40]");
-        /* save callee-saved registers */
-        __ write_inst("stp x20, x19, [x29, #-0x50]");
-        __ write_inst("stp x22, x21, [x29, #-0x60]");
-        __ write_inst("stp x24, x23, [x29, #-0x70]");
-        __ write_inst("stp x26, x25, [x29, #-0x80]");
-        __ write_inst("stp x28, x27, [x29, #-0x90]");
-        /* save floating registers */
-        __ write_inst("stp d9, d8, [x29, #-0xa0]");
-        __ write_inst("stp d11, d10, [x29, #-0xb0]");
-        __ write_inst( "stp d13, d12, [x29, #-0xc0]");
-        __ write_inst( "stp d15, d14, [x29, #-0xd0]");
-        // x7 holds thread, save it into global register x28
-        __ write_inst( "mov x28, x7");
-        // x3 holds method, save it into global register x12
-        __ write_inst( "mov x12, x3");
-        /* just disable compressed oops for now (-XX:-UseCompressedOops), handle it later */
-        /* setup java stack frame for parameters */
-        // save sp into x20
-        __ write_inst( "mov x20, sp");
-        // use this way a.
-        // SUB X8, SP, W6, UXTW #3 // X8 = SP - N*8
-        // AND SP, X8, #0xFFFFFFFFFFFFFFF0 // 16 bytes alignment
-        // or another way b.
-        // ADD W8, W6, #1 // W8 = N + 1
-        __ write_inst( "add w8, w6, #1");
-        // AND W8, W8, #0xFFFFFFFE // W8 &= ~1 // to even number
-        __ write_inst( "and w8, w8, #0xfffffffe");
-        // SUB SP, SP, W8, UXTW #3 // SP -= W8 * 8
-        __ write_inst( "sub sp, sp, w8, uxtw #3");
-        // way a. saves bytes than b., but may be less efficient than b.
-        /* push parameters into stack */
-        // jump to label parameters_done
+        __ write_inst("sub sp, x29, #%d", -sp_after_call_off * wordSize);
+
+        // save register parameters and Java scratch/global registers
+        // n.b. we save thread even though it gets installed in
+        // rthread because we want to sanity check rthread later
+        __ write_inst("str x7, [x29, #%d]", thread_off         * wordSize);
+        __ write_inst("str w6, [x29, #%d]", parameter_size_off * wordSize);
+        __ write_inst("stp x4, x5, [x29, #%d]", entry_point_off    * wordSize);
+        __ write_inst("stp x2, x3, [x29, #%d]", result_type_off    * wordSize);
+        __ write_inst("stp x0, x1, [x29, #%d]", call_wrapper_off   * wordSize);
+
+        __ write_inst("stp x20, x19, [x29, #%d]", r20_off * wordSize);
+        __ write_inst("stp x22, x21, [x29, #%d]", r22_off * wordSize);
+        __ write_inst("stp x24, x23, [x29, #%d]", r24_off * wordSize);
+        __ write_inst("stp x26, x25, [x29, #%d]", r26_off * wordSize);
+        __ write_inst("stp x28, x27, [x29, #%d]", r28_off * wordSize);
+
+        __ write_inst("stp d9, d8, [x29, #%d]", d9_off * wordSize);
+        __ write_inst("stp d11, d10, [x29, #%d]", d11_off * wordSize);
+        __ write_inst("stp d13, d12, [x29, #%d]", d13_off * wordSize);
+        __ write_inst("stp d15, d14, [x29, #%d]", d15_off * wordSize);
+
+        // install Java thread in global register now we have saved
+        // whatever value it held
+        __ write_inst("mov x28, x7");
+        // And method
+        __ write_inst("mov x12, x3");
+
+        // set up the heapbase register
+        // TODO
+//        __ reinit_heapbase();
+
+#ifdef ASSERT
+        // make sure we have no pending exceptions
+        {
+            YuhuLabel L;
+            __ write_inst("ldr x8, [x28, #%d]", in_bytes(Thread::pending_exception_offset()));
+            __ write_inst("cmp x8, #%d", (unsigned)NULL_WORD);
+            __ write_inst_b(__ eq, L);
+            __ write_insts_stop("YuhuStubRoutines::call_stub: entered with pending exception");
+            __ pin_label(L);
+        }
+#endif
+        // pass parameters if any
+        __ write_inst("mov x20, sp");
+        __ write_inst("sub x8, sp, w6, uxtw #%d", LogBytesPerWord); // Move SP out of the way
+        __ write_inst("and sp, x8, #%d", -2 * wordSize);
+
+//        BLOCK_COMMENT("pass parameters if any");
         YuhuLabel parameters_done;
-        // cbz w6, #20
+        // parameter count is still in c_rarg6
+        // and parameter pointer identifying param 1 is in c_rarg5
         __ write_inst_cbz(__ w6, parameters_done);
-        // -- label loop_start
-        address loop_start = __ current_pc();
-        // load parameter and move pointer forward + 8 bytes
-        __ write_inst( "ldr x8, [x5], #8");
-        // this updates NZCV flags
-        __ write_inst( "subs w6, w6, #0x1");
-        // str x8, [x20, #-8]! has the same effective as stur x8, [x20, #-8]! when using !
-        __ write_inst( "str x8, [x20, #-8]!");
-        // b.gt loop_start
-        // jump to label loop_start
-        // b.gt #-12
-        __ write_inst_b(__ gt, loop_start);
-        // -- label parameters_done
+
+        address loop = __ current_pc();
+        __ write_inst("ldr x8, [x5], #%d", wordSize);
+        __ write_inst("subs w6, w6, #1");
+        __ write_inst_push(__ x8);
+        __ write_inst_b(__ gt, loop);
+
         __ pin_label(parameters_done);
-        /* call java method */
-        // save sp in x13 (sender sp)
-        __ write_inst( "mov x13, sp");
-        __ write_inst( "blr x4");
+
+        // call Java entry -- passing methdoOop, and current sp
+        //      rmethod: Method*
+        //      r13: sender sp
+//        BLOCK_COMMENT("call Java function");
+        __ write_inst("mov x13, sp");
+        __ write_inst_blr(__ x4);
+
+        // we do this here because the notify will already have been done
+        // if we get to the next instruction via an exception
+        //
+        // n.b. adding this instruction here affects the calculation of
+        // whether or not a routine returns to the call stub (used when
+        // doing stack walks) since the normal test is to check the return
+        // pc against the address saved below. so we may need to allow for
+        // this extra instruction in the check.
+
+        // save current address for use by exception handling code
+
         return_address = __ current_pc();
-        /* store result depending on type */
-        // load result address into x3
-        __ write_inst( "ldur x3, [x29, #-56]");
-        // load result type into x2
-        __ write_inst( "ldur x2, [x29, #-48]");
-        YuhuLabel exit;
-        // this updates NZCV flags
-        // check result type is T_OBJECT
-        __ write_inst( "cmp x2, #%d", T_OBJECT);
-        // jump to check T_LONG
-        YuhuLabel check_long;
-        // b.ne #12
-        __ write_inst_b(__ ne, check_long);
-        // handle T_OBJECT
-        __ write_inst( "str x0, [x3]");
-        // jump to label exit
-        // b.al #56
-        __ write_inst_b(__ al, exit);
-        // check result type is T_LONG
-        __ pin_label(check_long);
-        __ write_inst( "cmp x2, #%d", T_LONG);
-        // jump to check T_FLOAT
-        YuhuLabel check_float;
-        // b.ne #12
-        __ write_inst_b(__ ne, check_float);
-        // handle T_LONG
-        __ write_inst( "str x0, [x3]");
-        // jump to label exit
-        // b.al #40
-        __ write_inst_b(__ al, exit);
-        // check result type is T_FLOAT
-        __ pin_label(check_float);
-        __ write_inst( "cmp x2, #%d", T_FLOAT);
-        // jump to check T_DOUBLE
-        YuhuLabel check_double;
-        // b.ne #12
-        __ write_inst_b(__ ne, check_double);
-        // handle T_FLOAT
-        __ write_inst( "str s0, [x3]");
-        // jump to label exit
-        // b.al #24
-        __ write_inst_b(__ al, exit);
-        // check result type is T_DOUBLE
-        __ pin_label(check_double);
-        __ write_inst( "cmp x2, #%d", T_DOUBLE);
-        // jump to handle result is T_INT
-        YuhuLabel check_int;
-        // b.ne #12
-        __ write_inst_b(__ ne, check_int);
-        // handle T_DOUBLE
-        __ write_inst( "str d0, [x3]");
-        // jump to label exit
-        // b.al #8
-        __ write_inst_b(__ al, exit);
-        // the result is T_INT for the rest of scenarios, and use 32-bytes register
-        // x3 holds result address
-        __ pin_label(check_int);
-        __ write_inst( "str w0, [x3]");
-        // -- label exit
+
+        // store result depending on type (everything that is not
+        // T_OBJECT, T_LONG, T_FLOAT or T_DOUBLE is treated as T_INT)
+        // n.b. this assumes Java returns an integral result in r0
+        // and a floating result in j_farg0
+        __ write_inst("ldr x3, [x29, #%d]", result_off         * wordSize);
+        YuhuLabel is_long, is_float, is_double, exit;
+        __ write_inst("ldr x2, [x29, #%d]", result_type_off    * wordSize);
+        __ write_inst("cmp x2, #%d", T_OBJECT);
+        __ write_inst_b(__ eq, is_long);
+        __ write_inst("cmp x2, #%d", T_LONG);
+        __ write_inst_b(__ eq, is_long);
+        __ write_inst("cmp x2, #%d", T_FLOAT);
+        __ write_inst_b(__ eq, is_float);
+        __ write_inst("cmp x2, #%d", T_DOUBLE);
+        __ write_inst_b(__ eq, is_double);
+
+        // handle T_INT case
+        __ write_inst("str w0, [x3]");
+
         __ pin_label(exit);
-        /* pop parameters */
-        // reassign x20 to pop java parameters
-        __ write_inst( "sub x20, x29, #0xd0");
-        /* restore callee-saved registers */
-        __ write_inst( "ldp d15, d14, [x29, #-208]");
-        __ write_inst( "ldp d13, d12, [x29, #-192]");
-        __ write_inst( "ldp d11, d10, [x29, #-176]");
-        __ write_inst( "ldp d9, d8, [x29, #-160]");
-        __ write_inst( "ldp x28, x27, [x29, #-144]");
-        __ write_inst( "ldp x26, x25, [x29, #-128]");
-        __ write_inst( "ldp x24, x23, [x29, #-112]");
-        __ write_inst( "ldp x22, x21, [x29, #-96]");
-        __ write_inst( "ldp x20, x19, [x29, #-80]");
-        __ write_inst( "ldp x0, x1, [x29, #-64]");
-        __ write_inst( "ldur w2, [x29, #-48]");
-        __ write_inst( "ldur x3, [x29, #-40]");
-        __ write_inst( "ldp x4, x5, [x29, #-32]");
-        __ write_inst( "ldp x6, x7, [x29, #-16]");
-        /* leave frame and return to caller */
+
+        // pop parameters
+        __ write_inst("sub x20, x29, #%d", -sp_after_call_off * wordSize);
+
+#ifdef ASSERT
+        // verify that threads correspond
+        {
+            YuhuLabel L, S;
+            __ write_inst("ldr x8, [x29, #%d]", thread_off         * wordSize);
+            __ write_inst("cmp x28, x8");
+            __ write_inst_b(__ ne, S);
+            __ write_insts_get_thread(__ x8);
+            __ write_inst("cmp x28, x8");
+            __ write_inst_b(__ eq, L);
+            __ pin_label(S);
+            __ write_insts_stop("YuhuStubRoutines::call_stub: threads must correspond");
+            __ pin_label(L);
+        }
+#endif
+
+        // restore callee-save registers
+        __ write_inst("ldp d15, d14, [x29, #%d]", d15_off * wordSize);
+        __ write_inst("ldp d13, d12, [x29, #%d]", d13_off * wordSize);
+        __ write_inst("ldp d11, d10, [x29, #%d]", d11_off * wordSize);
+        __ write_inst("ldp d9, d8, [x29, #%d]", d9_off * wordSize);
+
+        __ write_inst("ldp x28, x27, [x29, #%d]", r28_off * wordSize);
+        __ write_inst("ldp x26, x25, [x29, #%d]", r26_off * wordSize);
+        __ write_inst("ldp x24, x23, [x29, #%d]", r24_off * wordSize);
+        __ write_inst("ldp x22, x21, [x29, #%d]", r22_off * wordSize);
+        __ write_inst("ldp x20, x19, [x29, #%d]", r20_off * wordSize);
+
+        __ write_inst("ldp x0, x1, [x29, #%d]", call_wrapper_off   * wordSize);
+        __ write_inst("ldr w2, [x29, #%d]", result_type_off    * wordSize);
+        __ write_inst("ldr x3, [x29, #%d]", method_off         * wordSize);
+        __ write_inst("ldp x4, x5, [x29, #%d]", entry_point_off    * wordSize);
+        __ write_inst("ldp x6, x7, [x29, #%d]", parameter_size_off * wordSize);
+
+        // leave frame and return to caller
         __ write_insts_leave();
-        __ write_inst( "ret");
+        __ write_inst("ret");
+
+        // handle return types different from T_INT
+
+        __ pin_label(is_long);
+        __ write_inst("str x0, [x3, #0]");
+        __ write_inst_b(__ al, exit);
+
+        __ pin_label(is_float);
+        __ write_inst("str s0, [x3, #0]");
+        __ write_inst_b(__ al, exit);
+
+        __ pin_label(is_double);
+        __ write_inst("str d0, [x3, #0]");
+        __ write_inst_b(__ al, exit);
 
         // 使用 Disassembler 反汇编
         tty->print_cr("Disassembly:");
@@ -188,20 +251,41 @@ private:
     }
 
     address generate_catch_exception() {
+//        StubCodeMark mark(this, "StubRoutines", "catch_exception");
         address start = __ current_pc();
 
-        // x28 holds current thread
-        /* save exception */
-        __ write_inst( "str x0, [x28, #0x8]");
-        /* save file */
-        address imm64 = (address) __FILE__;
-        __ write_insts_mov_imm64(__ x8, (uint64_t)imm64);
-        __ write_inst( "str x8, [x28, #0x10]");
-        /* save line */
-        int imm32 = (int) __LINE__;
-        __ write_insts_mov_imm32(__ w8, (uint32_t)imm32);
-        __ write_inst( "str w8, [x28, #0x18]");
-        /* return to VM */
+        // same as in generate_call_stub():
+//        const Address sp_after_call(rfp, sp_after_call_off * wordSize);
+//        const Address thread        (rfp, thread_off         * wordSize);
+
+#ifdef ASSERT
+        // verify that threads correspond
+        {
+            YuhuLabel L, S;
+            __ write_inst("ldr x8, [x29, #%d]", thread_off         * wordSize);
+            __ write_inst("cmp x28, x8");
+            __ write_inst_b(__ ne, S);
+            __ write_insts_get_thread(__ x8);
+            __ write_inst("cmp x28, x8");
+            __ write_inst_b(__ eq, L);
+            __ pin_label(S);
+            __ write_insts_stop("YuhuStubRoutines::catch_exception: threads must correspond");
+            __ pin_label(L);
+        }
+#endif
+
+        // set pending exception
+        __ write_insts_verify_oop(__ x0, "broken oop");
+
+        __ write_inst("str x0, [x28, #%d]", in_bytes(Thread::pending_exception_offset()));
+        __ write_insts_mov_imm64(__ x8, (uint64_t)((address) __FILE__));
+        __ write_inst("str x8, [x28, #%d]", in_bytes(Thread::exception_file_offset()));
+        __ write_insts_mov_imm32(__ w8, (uint32_t)((int) __LINE__));
+        __ write_inst("str w8, [x28, #%d]", in_bytes(Thread::exception_line_offset()));
+
+        // complete return to VM
+        assert(YuhuStubRoutines::_call_stub_return_address != NULL,
+               "_call_stub_return_address must have been generated before");
         __ write_inst_b(YuhuStubRoutines::_call_stub_return_address);
 
         // 使用 Disassembler 反汇编
@@ -255,7 +339,7 @@ private:
         __ pin_label(error);
         __ write_inst( "ldp x3, x2, [sp], #0x10");
 
-        __ write_insts_pushrange(__ x0, __ x29, __ sp);
+        __ write_insts_push(YuhuRegSet::range(__ x0, __ x29), __ sp);
 
         // debug(char* msg, int64_t pc, int64_t regs[])
         __ write_inst_mov_reg(__ x0, __ x8); // pass address of error message
